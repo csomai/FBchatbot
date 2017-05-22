2837cdf1b71a9f74ad6901f5373f84c138f3309b
csomai <csomai.krisztian@gmail.com>
2017-05-22 22:26:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,45 +1,47 @@
-import os
-import requests
-from sys import argv
-from wit import Wit
-from bottle import Bottle, request, debug
-
 import sys
 from wit import Wit
 
-# Wit.ai parameters
-WIT_TOKEN = os.environ.get('SIRWNSUEZS2YM2PIQG27N2N4MVV35YVT')
-# Messenger API parameters
-FB_PAGE_TOKEN = os.environ.get('EAAaG4dbOagQBAPTwYpY6U1wTo31cZBSK1ZBuDn53MQlGZAJVwE8mRd0jx9RlszkQpZA300gcYk2d8JQNerofvlZAD79TtKOMNyjx0c5SQjcHryUDlKyls5goqXsGVSeXyh0tZCnEudubTjuo5JiR3FUxMZBuaZC73nZClDi1E9cZAFtgZDZD')
+if len(sys.argv) != 2:
+    print('usage: python ' + sys.argv[0] + ' <wit-token>')
+    exit(1)
+access_token = 'SIRWNSUEZS2YM2PIQG27N2N4MVV35YVT'
 
+# Quickstart example
+# See https://wit.ai/ar7hur/Quickstart
 
-# Setup Bottle Server
-debug(True)
-app = Bottle()
+def first_entity_value(entities, entity):
+    if entity not in entities:
+        return None
+    val = entities[entity][0]['value']
+    if not val:
+        return None
+    return val['value'] if isinstance(val, dict) else val
 
-# Facebook Messenger POST Webhook
-@app.post('/webhook')
-def messenger_post():
-    """
-    Handler for webhook (currently for postback and messages)
-    """
-    data = request.json
-    if data['object'] == 'page':
-        for entry in data['entry']:
-            # get all the messages
-            messages = entry['messaging']
-            if messages[0]:
-                # Get the first message
-                message = messages[0]
-                # Yay! We got a new message!
-                # We retrieve the Facebook user ID of the sender
-                fb_id = message['sender']['id']
-                # We retrieve the message content
-                text = message['message']['text']
-                # Let's forward the message to the Wit.ai Bot Engine
-                # We handle the response in the function send()
-                client.run_actions(session_id=fb_id, message=text)
+def send(request, response):
+    print(response['text'])
+
+    
+"""
+def get_forecast(request):
+    context = request['context']
+    entities = request['entities']
+
+    loc = first_entity_value(entities, 'location')
+    if loc:
+        context['forecast'] = 'sunny'
+        if context.get('missingLocation') is not None:
+            del context['missingLocation']
     else:
-        # Returned another event
-        return 'Received Different Event'
-return None
+        context['missingLocation'] = True
+        if context.get('forecast') is not None:
+            del context['forecast']
+
+    return context
+
+actions = {
+    'send': send,
+    'getForecast': get_forecast,
+"""
+
+client = Wit(access_token=access_token, actions=actions)
+client.interactive()
