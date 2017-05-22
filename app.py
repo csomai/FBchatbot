19e501c2dd71b1ea3cf0b5fe82f48ddab5f94062
csomai <csomai.krisztian@gmail.com>
2017-05-22 22:16:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,21 +7,6 @@ from bottle import Bottle, request, debug
 import sys
 from wit import Wit
 
-if len(sys.argv) != 2:
-    print('usage: python ' + sys.argv[0] + 'SIRWNSUEZS2YM2PIQG27N2N4MVV35YVT')
-    exit(1)
-access_token = 'SIRWNSUEZS2YM2PIQG27N2N4MVV35YVT'
-def send(request, response):
-    print(response['text'])
-
-actions = {
-    'send': send,
-}
-
-client = Wit(access_token=access_token, actions=actions)
-client.interactive()
-
-
 # Wit.ai parameters
 WIT_TOKEN = os.environ.get('WIT_TOKEN')
 # Messenger API parameters
